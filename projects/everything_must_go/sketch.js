ccd081564287a302e3598ec93c7b2001f4c4c897
Justin <jcrouch938@gmail.com>
2024-08-21 17:02:01 +0200

    vaults backup: 2024-08-21 17:02

--- projects/everything_must_go/sketch.js
+++ projects/everything_must_go/sketch.js
@@ -1,27 +1,39 @@
+const Engine	= Matter.Engine,
+      Runner	= Matter.Runner,
+      Bodies	= Matter.Bodies,
+      Composite = Matter.Composite;
+
+let engine = Engine.create();
+let ground = Bodies.rectangle(200, 350, 200, 50, {isStatic: true});
+let box1 = Bodies.rectangle(200, 200, 80, 80);
+
 let theta = 0;
 let thetaDelta;
 
 function setup() {
   createCanvas(400, 400);
-  thetaDelta = PI / 100;
+
+  Composite.add(engine.world, [ground, box1]);
+  
+  let runner = Runner.create();
+  Runner.run(runner, engine);
+
+//  thetaDelta = PI / 100;
 }
 
 function draw() {
-  background(255);
+  background(160);
 
   push();
     translate(200, 200);
     rotate(theta);
     beginShape();
-    for(let t=0; t<TWO_PI; t+=TWO_PI/100){
+    for(let t=0; t<TWO_PI; t+=TWO_PI/6){
       vertex(
-	50 *cos(t), 100*sin(t)
+	100*cos(t), 80*sin(t)
       );
     }
-    endShape();
-
-    fill(255, 0, 0, 100);
-    circle(0, 0, 100);
+    endShape(CLOSE);
   pop();
 
   theta += thetaDelta;
